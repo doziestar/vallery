5ec0a0becb7f5aa3accb2aa84831dfc5d92b46c5
doziestar <chidosiky2015@gmail.com>
2022-06-01 08:28:30 +0100

    starting vallery nft project

---
+++ vallery/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from vallery.users.models import User
+from vallery.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
