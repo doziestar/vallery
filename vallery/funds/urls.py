43365c851cd49192d69dcd65cc51b025ec080801
doziestar <chidosiky2015@gmail.com>
2022-06-06 21:19:43 +0100

    coinbase api

--- vallery/funds/urls.py
+++ vallery/funds/urls.py
@@ -1,3 +1,11 @@
-app_name = "funds"
+from django.urls import path
 
-urlpatterns = []
+from vallery.funds import views
+
+app_name = "funds"
+urlpatterns = [
+    path("", views.home_view, name="payments-home"),
+    path("success/", views.success_view, name="payments-success"),
+    path("cancel/", views.cancel_view, name="payments-cancel"),
+    path("webhook/", views.coinbase_webhook),
+]
